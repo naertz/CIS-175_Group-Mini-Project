4db6e2d75d7e6be27438d385d9bb585ea02251f2
Noah Allan Ertz <naertz@dmacc.edu>
2021-10-15 17:13:54 -0500

    Added edit-task.jsp and EditTaskServlet

---
+++ src/main/java/controller/EditTaskServlet.java
@@ -0,0 +1,68 @@
+package controller;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Person;
+import model.Task;
+
+/**
+ * Servlet implementation class EditTaskServlet
+ */
+@WebServlet("/EditTaskServlet")
+public class EditTaskServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditTaskServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    TaskHelper th = new TaskHelper();
+        
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        Task task = th.searchForTasksById(id);
+	    
+	    String personName = request.getParameter("personName");
+        Person person = new Person(personName);
+        task.setPerson(person);
+        
+        String name = request.getParameter("name");
+        task.setName(name);
+        
+        String month = request.getParameter("month");
+        String day   = request.getParameter("day");
+        String year  = request.getParameter("year");
+        
+        LocalDate dueDate;
+        try {
+            dueDate = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        } catch (NumberFormatException error) {
+            dueDate = LocalDate.now();
+        }
+		task.setDueDate(dueDate);
+		
+		th.updateTask(task);
+		
+		getServletContext().getRequestDispatcher("/ViewAllTasksServlet").forward(request, response);
+	}
+}
---
+++ src/main/webapp/edit-task.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" href="#">
+        <title>Tasklist - Edit Task</title>
+    </head>
+    <body>
+        <h1>Edit the Task:</h1>
+        <form action="EditTaskServlet" method="post">
+            <fieldset>
+                <legend>Enter the person's name this task is assigned to:</legend>
+                <input type="text" name="personName" value="${newTask.person.name}">
+            </fieldset>
+            <fieldset>
+                <legend>Enter the task's name:</legend>
+                <input type="text" name="name" value="${newTask.name}">
+            </fieldset>
+            <fieldset>
+                <legend>Enter the task's due date:</legend>
+                <input type="text" name="month" placeholder="mm" size="4" value="${month}">
+                <input type="text" name="day" placeholder="dd" size="4" value="${date}">
+                <input type="text" name="year" placeholder="yyyy" size="4" value="${year}">
+            </fieldset>
+            <fieldset>
+                <legend>Update the task:</legend>
+                <input type="hidden" name="id" value="${newTask.id}">
+                <input type="submit" value="Save">
+            </fieldset>
+        </form>
+    </body>
+</html>
