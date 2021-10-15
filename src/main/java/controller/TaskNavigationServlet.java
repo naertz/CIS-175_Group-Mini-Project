926f791eb33aad8a5e8f722a09021466980953b0
Noah Allan Ertz <naertz@dmacc.edu>
2021-10-15 15:31:01 -0500

    Added ViewAllTasksServlet, tasklist.jsp, and TaskNavigationServlet

---
+++ src/main/java/controller/TaskNavigationServlet.java
@@ -0,0 +1,67 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Task;
+
+/**
+ * Servlet implementation class TaskNavigationServlet
+ */
+@WebServlet("/TaskNavigationServlet")
+public class TaskNavigationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TaskNavigationServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		TaskHelper th = new TaskHelper();
+		
+		String action = request.getParameter("performTaskAction");
+		
+		String path = "/ViewAllTasksServlet";
+		
+		if (action.equals("Add Task")) {
+		    path = "/index.html";
+		} else if (action.equals("Edit Task")) {
+		    try {
+		        Integer id = Integer.parseInt(request.getParameter("id"));
+		        Task task = th.searchForTasksById(id);
+		        request.setAttribute("newTask", task);
+		        path = "/edit-task.jsp";
+		    } catch (NumberFormatException error) {
+		        System.out.println("Missing task");
+		    }
+		} else if (action.equals("Delete Task")) {
+		    try {
+		        Integer id = Integer.parseInt(request.getParameter("id"));
+		        Task task = th.searchForTasksById(id);
+		        th.deleteTask(task);
+		    } catch (NumberFormatException error) {
+		        System.out.println("Missing task");
+		    }
+		}
+		
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/tasklist.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" href="#">
+        <title>Tasklist - Tasklist</title>
+    </head>
+    <body>
+        <form action="TaskNavigationServlet" method="post">
+            <table>
+                <c:forEach items="${requestScope.taskList}" var="currentTask">
+                    <tr>
+                        <td><input type="radio" id="task" name="id" value="${currentTask.id}"><label for="task">${currentTask.name}</label></td>
+                    </tr>
+                    <tr>
+                        <td>Assigned to:</td>
+                        <td>${currentTask.person.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Due:</td>
+                        <td>${currentTask.dueDate}</td>
+                    </tr>
+                </c:forEach>
+            </table>
+            <input type="submit" value="Add Task" name="performTaskAction">
+            <input type="submit" value="Edit Task" name="performTaskAction">
+            <input type="submit" value="Delete Task" name="performTaskAction">
+        </form>
+    </body>
+</html>
