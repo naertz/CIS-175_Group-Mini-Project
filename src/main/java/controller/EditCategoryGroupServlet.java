c310f2f113eee4f32c9828a15ad56bc9bc76226d
Noah Allan Ertz <naertz@dmacc.edu>
2021-10-15 21:58:11 -0500

    Added edit-category-group.jsp and EditCategoryGroupServlet

---
+++ src/main/java/controller/EditCategoryGroupServlet.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Category;
+import model.Task;
+
+/**
+ * Servlet implementation class EditCategoryGroupServlet
+ */
+@WebServlet("/EditCategoryGroupServlet")
+public class EditCategoryGroupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditCategoryGroupServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CategoryHelper ch = new CategoryHelper();
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		Category category = ch.searchForCategoriesById(id);
+		
+		String name = request.getParameter("name");
+		category.setName(name);
+		
+		TaskHelper th = new TaskHelper();
+		List<Task> tasks = new ArrayList<Task>();
+		try {
+		    String[] taskIds = request.getParameterValues("tasks");
+		    for (int i = 0; i < taskIds.length; ++i) {
+		        Task task = th.searchForTasksById(Integer.parseInt(taskIds[i]));
+		        tasks.add(task);
+		    }
+		} catch (NullPointerException error) {
+		    tasks = category.getTasks();
+		}
+		category.setTasks(tasks);
+		
+		ch.updateCategory(category);
+		
+		getServletContext().getRequestDispatcher("/ViewAllCategoryGroupsServlet").forward(request, response);
+	}
+}
---
+++ src/main/webapp/edit-category-group.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" href="#">
+        <title>Tasklist - Edit Category Group</title>
+    </head>
+    <body>
+        <h1>Edit the Category Group:</h1>
+        <form action="UpdateHopExperienceServlet" method="post">
+            <fieldset>
+                <legend>Enter the category group's name:</legend>
+                <input type="text" name="name" value="${newCategory.name}">
+            </fieldset>
+            <fieldset>
+                <legend>Add tasks to the category group:</legend>
+                <select name="tasks" multiple size="16">
+                    <c:forEach var="task" items="${requestScope.tasks}">
+                        <option value="${task.id}">${task.name} | Assigned to: ${task.person} | Due: ${task.dueDate}</option>
+                    </c:forEach>
+                </select>
+            </fieldset>
+            <fieldset>
+                <legend>Submit the edited category group:</legend>
+                <input type="hidden" name="id" value="${newCategory.id}">
+                <input type="submit" value="Submit">
+            </fieldset>
+        </form>
+        <a href="index.html">Add tasks</a>
+    </body>
+</html>
